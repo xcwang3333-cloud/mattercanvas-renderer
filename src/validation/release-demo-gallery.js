fba9e1c1188df613a76b10834b286686f1fd21bf
xiaochen wang <xcwang3333@gmail.com>
2026-09-17 20:49:52 +0800

    feat: add release demo gallery manifest

---
+++ src/validation/release-demo-gallery.js
@@ -0,0 +1,21 @@
+export const RELEASE_DEMO_GALLERY = [
+  {
+    id: 'PbN4_CO2RR',
+    template: 'JACS',
+    panels: [
+      'PbN4',
+      'PbN4-COOH',
+      'PbN4-OCHO',
+      'PbN4-CO'
+    ],
+    highlight: ['Pb']
+  },
+  {
+    id: 'LnFe_ORR',
+    template: 'Nature',
+    panels: [
+      'LnFe-N-C'
+    ],
+    highlight: ['Fe', 'La', 'Ce', 'Gd']
+  }
+];
